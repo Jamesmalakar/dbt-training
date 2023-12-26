6cf8f8c2769cb38c4df99728c39da5c3b212c8b6
Janak Mali <Janakmali73@gmail.com>
2023-12-26 21:04:39 +0000

    developing staging folders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,32 @@
+select 
+-- from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice,
+o.ordercostprice,
+o.ordersellingprice - o.ordercostprice as orderprofit 
+, c.customername
+, c.segment
+, c.country
+, p.category
+, p.productname
+, p.subcategory
+from 
+{{ ref('raw_orders') }} as o 
+left join {{ref('raw_customers')}} as c 
+On o.customerid = c.customerid
+left join {{ref('raw_products')}} as p 
+On o.productid = p.productid
+
+
+
+
+
+
+
+
+
+
+
